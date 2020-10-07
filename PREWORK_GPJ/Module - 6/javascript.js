1625fd0381f0d4ff49f01f40a98ea058e18691e6
Grantjos12 <grantjoslyn@gmail.com>
2020-10-06 22:16:40 -0500

    this is the pre-work, which was not able to be acessed through google docs

---
+++ PREWORK_GPJ/Module - 6/javascript.js
@@ -0,0 +1,22 @@
+
+
+
+function grow() {
+  document.getElementById("button1").style.height = "400px";
+  }
+function blue() {
+    document.getElementById("button2").style.backgroundColor = "blue";
+  }
+
+function fade() {
+    document.getElementById("button3").style.opacity = "0.6";
+  }
+  
+function reset() {
+    document.getElementById("button4").style.height = "150px";
+    document.getElementById("button4").style.backgroundColor = "orange";
+    document.getElementById("button4").style.opacity = "1";
+  
+  }
+
+ 
